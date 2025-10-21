dde0ce94091b5fa35f86d7a5d18f0ff17eb0222b
Roman <romazan.26@gmail.com>
2025-10-21 20:02:12 +0300

    apsf

--- EggCrossingKit/AppDelegate.swift
+++ EggCrossingKit/AppDelegate.swift
@@ -10,36 +10,36 @@ import AppsFlyerLib
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var restrictRotation: UIInterfaceOrientationMask = .all
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // AppsFlyer Init
-           AppsFlyerLib.shared().appsFlyerDevKey = "suQJTxXVai2ULmaTSDrt2W"
-           AppsFlyerLib.shared().appleAppID = "6749801154"
-           AppsFlyerLib.shared().delegate = self
-           AppsFlyerLib.shared().isDebug = true
-           
+        AppsFlyerLib.shared().appsFlyerDevKey = "suQJTxXVai2ULmaTSDrt2W"
+        AppsFlyerLib.shared().appleAppID = "6749801154"
+        AppsFlyerLib.shared().delegate = self
+        AppsFlyerLib.shared().isDebug = true
+        AppsFlyerLib.shared().disableAdvertisingIdentifier = true
+        
         AppsFlyerLib.shared().start()
-        let appsFlyerId = AppsFlyerLib.shared().getAppsFlyerUID()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
